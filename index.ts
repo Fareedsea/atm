aa45fc71e9d8baef3840f76db6b977858684fda2
Fareedsea <fareed.sea@gmail.com>
2024-03-28 14:03:32 +0500

    Add more option in files

--- index.ts
+++ index.ts
@@ -11,24 +11,43 @@ let pinanswers = await inquirer.prompt(
 );
 if (pinanswers.userPin === myPin) {
     console.log("Your Enter Pin Code is Correct:");
+        const choice = await inquirer.prompt([
+            {
+                message: "Select your option to perform action:",
+                type: "list",
+                name: "selection",
+                choices: ["Withdrawl", "Balance"],
+            },
+            ]
+
+        )
+        if (choice.selection === "Withdrawl") {
+            
+                let withdarwalAmt = await inquirer.prompt(
+                    {
+                    name: "amt",
+                    type: "number",
+                    message: "Please Enter Your Withdrawl Amount :",
+                    }
+                        );
+                        if (withdarwalAmt.amt <= myBalance){
+                            console.log(choice.selection);
+                            console.log(withdarwalAmt.amt);
+                            myBalance = myBalance - withdarwalAmt.amt
+                            console.log("Now Your Account Balance is Rs.", myBalance);
+                                      
+                        } else {
+                            console.log("Your Account Balance is Short:", myBalance);
+
+                        }
+
+                  
+        } else {
+            console.log(choice.selection);
+        }
 
 } else {
     console.log("Your Enter Pin Code is Incorrect:");
 }
-// if (pinanswers.userPin === myPin) {
-//     let optionChoice = await inquirer.prompt([
-//         {
-//             message: "Select your option to perform action:",
-//             type: "list",
-//             name: "selection",
-//             choices: ["Balance", "Withdrawl"],
-//           },
-//         ]
-
-//     )};      
-
-// } else {
-//     console.log("Your Enter Pin Code is Incorrect:");
-// },
-
+console.log("Last line Your Account Balance is Rs.", myBalance );
 console.log(pinanswers.userPin);
